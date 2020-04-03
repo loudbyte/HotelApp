d00330351af6d82cc740cb7ebf0cff1be1b3e943
mikhail <davebottomhell@gmail.com>
2020-04-03 23:55:58 +0600

    DAO updated

--- src/main/java/com/epam/hotel/dao/daocommon/RoomDAOInterface.java
+++ src/main/java/com/epam/hotel/dao/daocommon/RoomDAOInterface.java
@@ -2,7 +2,6 @@ package com.epam.hotel.dao.daocommon;
 
 import com.epam.hotel.entity.Room;
 
-
 public interface RoomDAOInterface extends BaseDAOInterface<Room> {
 
     Room getByRoomNumber(int roomNumber);
